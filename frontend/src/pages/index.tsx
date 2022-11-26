25df40b3b8e1c2d78ff2122c03c0a4f9ddce9c9c
Martin Zokov <martinzokov@gmail.com>
2022-11-26 14:34:29 +0000

    Add loading indicator to index

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -9,14 +9,18 @@ import { getSubscriptionDetails } from "@/client/BackendClient";
 const Index = () => {
   const isAuthenticated = useIsAuthenticated();
   const [hasActiveSubscription, setHasActiveSubscription] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     if (isAuthenticated) {
       getSubscriptionDetails()
         .then((response) => {
           setHasActiveSubscription(response.data.isActive);
+          setIsLoading(false);
         })
         .catch((response) => {
+          setIsLoading(true);
           console.error(response);
         });
     }
@@ -31,10 +35,18 @@ const Index = () => {
         />
       }
     >
-      {isAuthenticated && hasActiveSubscription && (
+      {isLoading && (
+        <div className="flex justify-center">
+          <div className="w-10 h-10 animate-ping rounded-full bg-main"></div>
+        </div>
+      )}
+
+      {isAuthenticated && hasActiveSubscription && !isLoading && (
         <div>Welcome to ZippySaaS!</div>
       )}
-      {isAuthenticated && !hasActiveSubscription && <PriceSelection />}
+      {isAuthenticated && !hasActiveSubscription && !isLoading && (
+        <PriceSelection />
+      )}
     </Main>
   );
 };
